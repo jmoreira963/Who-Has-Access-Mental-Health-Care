f2d56ce9161a9c34f567df24e9219bcb9894caf2
Jacqueline Moreira <jacqueline.moreira963@gmail.com>
2026-06-17 21:22:42 -0600

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -2,3 +2,12 @@
 -- A Data Analysis of Mental Health Care Availability in the United States
 -- Dataset: HRSA Mental Health Professional Shortage Area Data
 -- Purpose: Analyze mental health provider shortages, underserved populations, and rural access disparities.
+## Basic Preview Query
+SELECT *
+FROM `mental-health-hpsa.mental_health_hpsa.Project-2`
+LIMIT 10;
+## Count Total Records
+SELECT
+  COUNT(*) AS total_records
+FROM `mental-health-hpsa.mental_health_hpsa.Project-2`
+
